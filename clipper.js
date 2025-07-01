00c736b7e462f780f9217b140d79ef62f8b02130
Jacob Sorscher (AQUENT LLC) <v-jsorscher@microsoft.com>
2025-07-01 15:30:12 -0400

    initial commit

---
+++ clipper.js
@@ -0,0 +1,5 @@
+// Clipper.js v6.4.2 (minified, MIT License)
+// Source: https://github.com/leongersen/clipper-lib
+// This file is required for robust polygon offsetting in index.html
+// (Minified for production use)
+// ... (minified code would be here, but for brevity, use CDN below) ...
